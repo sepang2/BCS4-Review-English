de612e1cbc02d8efb0a8d3404280b25a7e03c3f8
Sepang2 <sepang_2@naver.com>
2023-11-29 16:22:52 +0900

    add TTS

--- src/pages/day.jsx
+++ src/pages/day.jsx
@@ -1,12 +1,21 @@
 import { Link, useParams } from "react-router-dom";
 import englishData from "../englishData.json";
 import { useEffect, useState } from "react";
-import { FiHome } from "react-icons/fi";
+import {
+  FiHome,
+  FiChevronsRight,
+  FiChevronsLeft,
+  FiVolume2,
+  FiSlash,
+} from "react-icons/fi";
+import axios from "axios";
 
 const Day = () => {
   const [dailyData, setDailyData] = useState();
   const [isVisible, setIsVisible] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
+  const [speakingRate, setspeakingRate] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
   const { day } = useParams();
 
   const onClickNext = () => {
@@ -16,7 +25,53 @@ const Day = () => {
   const onClickPrev = () => {
     if (currentPage > 0) setCurrentPage(currentPage - 1);
   };
-  const onClickSound = () => {};
+  const onClickSpeed = () => {
+    if (speakingRate === 1) setspeakingRate(2);
+    if (speakingRate === 2) setspeakingRate(1);
+  };
+  const onClickSound = async () => {
+    try {
+      setIsLoading(true);
+      if (isLoading) return;
+
+      const response = await axios.post(
+        `https://texttospeech.googleapis.com/v1/text:synthesize?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          input: {
+            text: dailyData.sentences[currentPage].english,
+          },
+          voice: {
+            languageCode: "en-gb",
+            name: "en-GB-Standard-A",
+            ssmlGender: "FEMALE",
+          },
+          audioConfig: {
+            audioEncoding: "MP3",
+            speakingRate: speakingRate,
+            pitch: 0,
+          },
+        }
+      );
+      const binaryData = atob(response.data.audioContent); // sound data를 binary data로
+
+      const byteArray = new Uint8Array(binaryData.length); // binary data를 byte array에 담기
+      for (let i = 0; i < binaryData.length; i++) {
+        byteArray[i] = binaryData.charCodeAt(i);
+      }
+
+      const blob = new Blob([byteArray.buffer], { type: "audio/mp3" }); // audio file로 변환
+      const newAudio = new Audio(URL.createObjectURL(blob));
+
+      document.body.appendChild(newAudio);
+      newAudio.play();
+
+      setTimeout(() => setIsLoading(false), 5000);
+    } catch (error) {
+      console.error(error);
+
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     englishData.forEach((v) => {
@@ -46,16 +101,41 @@ const Day = () => {
         >
           {dailyData.sentences[currentPage].korean}
         </button>
-        <div className="mt-4">
-          <button className="btn-style" onClick={onClickPrev}>
-            Prev
-          </button>
-          <button className="btn-style ml-2" onClick={onClickNext}>
-            Next
-          </button>
-          <button className="btn-style ml-2" onClick={onClickSound}>
-            Sound
-          </button>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <button
+              className={`btn-style ${
+                currentPage === 0 && "hover:border-red-500 hover:text-red-500"
+              }`}
+              onClick={onClickPrev}
+            >
+              {currentPage === 0 ? <FiSlash /> : <FiChevronsLeft />}
+            </button>
+            <button
+              className={`btn-style ml-2 ${
+                currentPage === dailyData.sentences.length - 1 &&
+                "hover:border-red-500 hover:text-red-500"
+              }`}
+              onClick={onClickNext}
+            >
+              {currentPage === dailyData.sentences.length - 1 ? (
+                <FiSlash />
+              ) : (
+                <FiChevronsRight />
+              )}
+            </button>
+          </div>
+          <div>
+            <button className="btn-style" onClick={onClickSpeed}>
+              {`${speakingRate === 1 ? "x1" : "x2"}`}
+            </button>
+            <button
+              className="btn-style ml-2 hover:border-blue-500 hover:text-blue-500"
+              onClick={onClickSound}
+            >
+              <FiVolume2 />
+            </button>
+          </div>
         </div>
       </div>
     </div>
