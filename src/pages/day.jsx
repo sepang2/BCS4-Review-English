dba749085af5b83a8839221518de14a5a2c68ca1
Sepang2 <sepang_2@naver.com>
2023-11-29 13:55:40 +0900

    Day page

--- src/pages/day.jsx
+++ src/pages/day.jsx
@@ -1,9 +1,65 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import englishData from "../englishData.json";
+import { useEffect, useState } from "react";
+import { FiHome } from "react-icons/fi";
 
 const Day = () => {
+  const [dailyData, setDailyData] = useState();
+  const [isVisible, setIsVisible] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
   const { day } = useParams();
 
-  return <div>Day {day}</div>;
+  const onClickNext = () => {
+    if (currentPage < dailyData.sentences.length - 1)
+      setCurrentPage(currentPage + 1);
+  };
+  const onClickPrev = () => {
+    if (currentPage > 0) setCurrentPage(currentPage - 1);
+  };
+  const onClickSound = () => {};
+
+  useEffect(() => {
+    englishData.forEach((v) => {
+      if (v.day === +day) setDailyData(v);
+    });
+  }, [day]);
+
+  useEffect(() => console.log(dailyData), [dailyData]);
+
+  if (!dailyData) return <div>Loading...</div>;
+
+  return (
+    <div className="container relative">
+      <div className="absolute top-0 left-0 p-8">
+        <Link to="/" className="text-3xl hover:text-gray-400">
+          <FiHome />
+        </Link>
+      </div>
+      <h1 className="text-center text-2xl font-semibold">
+        Day {dailyData.day} - {dailyData.title}
+      </h1>
+      <div className="mt-12">
+        <div>{dailyData.sentences[currentPage].english}</div>
+        <button
+          className={`${!isVisible && "bg-black"}`}
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          {dailyData.sentences[currentPage].korean}
+        </button>
+        <div className="mt-4">
+          <button className="btn-style" onClick={onClickPrev}>
+            Prev
+          </button>
+          <button className="btn-style ml-2" onClick={onClickNext}>
+            Next
+          </button>
+          <button className="btn-style ml-2" onClick={onClickSound}>
+            Sound
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Day;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,7 +3,7 @@ import englishData from "../englishData.json";
 
 const Main = () => {
   return (
-    <div className="min-h-screen max-w-screen-md mx-auto px-8 pt-20">
+    <div className="container">
       <h1 className="text-center font-black text-4xl">🇺🇸 Study English</h1>
       <ul className="mt-12 flex flex-col gap-8 pb-20">
         {englishData.map((v, i) => (
